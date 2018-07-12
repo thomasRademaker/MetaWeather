58f6f3b3888828cb12dc0267c6367aaf784bd356
Thomas Rademaker <thomas@rademaker.io>
2018-07-12 19:57:03 -0400

    View Controllers and Views created
    Weather view and view controller
    history view and view controller
    fiveDay view and view controller
    
    ViewController.swift deleted

---
+++ MetaWeather/Views/WeatherView.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherView.swift
+//  MetaWeather
+//
+//  Created by Thomas Rademaker on 7/12/18.
+//  Copyright © 2018 Thomas J. Rademaker. All rights reserved.
+//
+
+import UIKit
+
+class WeatherView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
