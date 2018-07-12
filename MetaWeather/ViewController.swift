70e64a07f0a434428a6bfb998e80e7d800b67ca2
Thomas Rademaker <thomas@rademaker.io>
2018-07-12 19:38:22 -0400

    Initial Commit

---
+++ MetaWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetaWeather
+//
+//  Created by Thomas Rademaker on 7/12/18.
+//  Copyright © 2018 Thomas J. Rademaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
