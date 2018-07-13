2e7b6d2114d7daeef7ac47fe448fbab20d90d1a1
Thomas Rademaker <thomas@rademaker.io>
2018-07-13 10:47:21 -0400

    HistoryViewController converted to a tableViewController

---
+++ MetaWeather/Views/HistoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryCell.swift
+//  MetaWeather
+//
+//  Created by Thomas Rademaker on 7/13/18.
+//  Copyright © 2018 Thomas J. Rademaker. All rights reserved.
+//
+
+import UIKit
+
+class HistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
