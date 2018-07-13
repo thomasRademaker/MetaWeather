9bf3bb126819220dd2d88eccce6779b84106d756
Thomas Rademaker <thomas@rademaker.io>
2018-07-12 22:09:49 -0400

    Weather and Location Models created

---
+++ MetaWeather/Models/Location.swift
@@ -0,0 +1,26 @@
+//
+//  Location.swift
+//  MetaWeather
+//
+//  Created by Thomas Rademaker on 7/12/18.
+//  Copyright © 2018 Thomas J. Rademaker. All rights reserved.
+//
+
+import Foundation
+
+struct Location: Codable {
+    
+    let distance: Int
+    let title: String
+    let locationType: String
+    let woeid: Int
+    let lattLong: String
+    
+    enum CodingKeys: String, CodingKey {
+        case distance
+        case title
+        case locationType = "location_type"
+        case woeid
+        case lattLong = "latt_long"
+    }
+}
---
+++ MetaWeather/Models/Weather.swift
@@ -0,0 +1,54 @@
+//
+//  Weather.swift
+//  MetaWeather
+//
+//  Created by Thomas Rademaker on 7/12/18.
+//  Copyright © 2018 Thomas J. Rademaker. All rights reserved.
+//
+
+import Foundation
+
+struct Weather: Codable {
+    
+    let id: Int
+    let weatherStateName: String
+    let weatherStateAbbr: String
+    let windDirectionCompass: String
+    let created: String
+    let applicableDate: String
+    let minTemp: Double
+    let maxTemp: Double
+    let theTemp: Double
+    let windSpeed: Double
+    let windDirection: Double
+    let airPressure: Double
+    let humidity: Int
+    let visibility: Double
+    let predictability: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case weatherStateName = "weather_state_name"
+        case weatherStateAbbr = "weather_state_abbr"
+        case windDirectionCompass = "wind_direction_compass"
+        case created
+        case applicableDate = "applicable_date"
+        case minTemp = "min_temp"
+        case maxTemp = "max_temp"
+        case theTemp = "the_temp"
+        case windSpeed = "wind_speed"
+        case windDirection = "wind_direction"
+        case airPressure = "airPressure"
+        case humidity
+        case visibility
+        case predictability
+    }
+}
+
+struct ConsolidatedWeather: Codable {
+    let weathers: [Weather]
+    
+    enum CodingKeys: String, CodingKey {
+        case weathers = "consolidated_weather"
+    }
+}
