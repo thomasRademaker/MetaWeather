84f92e2b997d27909d81ebb75a80c265fd0c2be3
Thomas Rademaker <thomas@rademaker.io>
2018-07-16 18:09:01 -0400

    Search+CoreDataClass.swift file was not being tracked by git

---
+++ MetaWeather/CoreData/Search+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Search+CoreDataClass.swift
+//  
+//
+//  Created by Thomas Rademaker on 7/13/18.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Search)
+public class Search: NSManagedObject {
+
+}
